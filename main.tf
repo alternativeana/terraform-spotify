b32f1ed5f69b54e82a2d680e59683c13a0e05083
Ana Harrison <ana@anana.dev>
2021-08-05 13:50:58 +0100

    Initial. Creates the Terraformering playlist 🎵

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    spotify = {
+      version = "~> 0.1.5"
+      source  = "conradludgate/spotify"
+    }
+  }
+}
+
+provider "spotify" {
+  api_key = var.spotify_api_key
+}
+
+data "spotify_search_track" "by_artist" {
+  artists = ["Thank You Scientist"]
+  album = "Terraformer"
+}
+
+resource "spotify_playlist" "playlist" {
+  name        = "Terraforming"
+  description = "Playlist created by Terraform. Terraformers only 🙅🏼‍♀️"
+  public      = true
+
+  tracks      = data.spotify_search_track.by_artist.tracks[*].id
+}
+
+
+output "tracks" {
+  value = data.spotify_search_track.by_artist.tracks
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Spotify Terraform Playlists
+
+Creating spotify playlist in the most superfluous manor possible. Very cool.
+
+A small repository for learning some terraform basics and listening to good music at the same time!
+
+--- 
+
+Hey look, a [playlist](https://open.spotify.com/playlist/3xpywptzO5CR0W09rGvQn3)!
